9a979ae35eb866bfc0eaaf6a47d5f0271f15a4f6
Kimberly Phan <kimberly.t.phan01@sjsu.edu>
2023-06-19 03:08:56 -0700

    add activity form

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from "react";
 import { fetchFromAPI } from '../api';
 
 const Activities = ({
-  token,
-  user
+  token
 }) => {
 
   const [activities, setActivities] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [name, setName] = useState([]);
+  const [description, setDescription] = useState([]);
+
   const fetchActivities = async () => {
     const data = await fetchFromAPI({
       endpoint: "activities",
@@ -19,6 +23,32 @@ const Activities = ({
     }
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const result = await fetchFromAPI({
+      body: {
+        name,
+        description
+      },
+      method: 'post',
+      endpoint: 'activities',
+      token
+    });
+
+    if (result.message) {
+      setErrorMessage(result.message)
+    }
+    else {
+      fetchActivities();
+    }
+
+    setName('');
+    setDescription('');
+
+  }
+
+
+
   useEffect(() => {
     fetchActivities();
     console.log("activities: ", activities)
@@ -29,7 +59,32 @@ const Activities = ({
   return (
     <>
       <h2><u>Activities</u></h2>
-      
+
+      {token &&
+        <form onSubmit={handleSubmit} className='addActivityForm'>
+          {errorMessage && <p className='errorMessage'>{errorMessage}</p>}
+          <input
+            type='text'
+            name='activityName'
+            placeholder='name'
+            value={name}
+            onChange={event => setName(event.target.value)}
+            minLength='3'
+            maxLength='20'
+            required
+          />
+          <input
+            type='text'
+            name='activityDescription'
+            placeholder='description'
+            value={description}
+            onChange={event => setDescription(event.target.value)}
+            required
+          />
+          <button type='submit'>Add Activity</button>
+        </form>
+      }
+
       <div className="displayActivities">
         {activities.map((activity, idx) => (
           <div key={activity.id ?? idx}>
